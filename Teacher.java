ee276301911184aee8688ce6d7e9b5b15a05887b
Michael DeVito <mdevitoii04@gmail.com>
2025-04-11 16:29:32 -0400

    Update Teacher.java

--- Teacher.java
+++ Teacher.java
@@ -1,3 +1,23 @@
-public class Teacher {
+// Michael DeVito II
+
+public class Teacher extends Person { // Child Class
     
+    private final String subject;
+    private final int yearsOfExperience;
+
+    public Teacher(String name, int age, String subject, int yearsOfExperience) {
+        super(name, age);
+        this.subject = subject;
+        this.yearsOfExperience = yearsOfExperience;
+    }
+
+    @Override
+    public String introduce() {
+        return super.introduce() + " I teach " + this.subject + " and I have " + this.yearsOfExperience + " years of experience.";
+    }
+
+    public boolean isVeteran() {
+        return (yearsOfExperience >= 10);
+    }
+
 }
