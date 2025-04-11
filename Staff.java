d1c99233041b10d87ab082f6e834df22a113cf3d
Michael DeVito <mdevitoii04@gmail.com>
2025-04-11 16:31:35 -0400

    Update Staff.java

--- Staff.java
+++ Staff.java
@@ -1,3 +1,18 @@
-public class Staff {
+// Michael DeVito II
+
+public class Staff extends Person { // Child Class
+    
+    private final String department;
+
+    public Staff(String name, int age, String department) {
+        super(name, age);
+        this.department = department;
+    }
+
+    @Override
+    public String introduce() {
+        return super.introduce() + " I work in the " + this.department + " department.";
+    }
+
     
 }
